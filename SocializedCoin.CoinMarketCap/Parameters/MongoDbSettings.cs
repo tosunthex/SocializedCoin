4c95ff958d0f47e66ee7fb98432e06056269f2cf
Levent Saatci <leventsaatci@gmail.com>
2018-10-17 10:19:33 +0200

    rider error dump commit

---
+++ SocializedCoin.CoinMarketCap/Parameters/MongoDbSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace SocilizedCoin.CoinMarketCap.Parameters
+{
+    public class MongoDbSettings
+    {
+        public static string ConnectionString => "mongodb://admin:abc123!@localhost";
+        public static string CoinmarketcapDatabase => "CoinMarketCap";
+        public static string CryptocompareDatabase => "CryptoCompare";
+    }
+}
